0870252ebf879f0319442c6b59c33311c2d290f7
Daniel Bubenheim <dbubenheim@communicode.de>
2016-08-31 10:10:53 +0200

    adds descriptions to the classes

--- src/main/java/edu/dbubenheim/projecteuler/MultiplesBelow3And5.java
+++ src/main/java/edu/dbubenheim/projecteuler/MultiplesBelow3And5.java
@@ -3,6 +3,16 @@ package edu.dbubenheim.projecteuler;
 import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 
+/**
+ * Problem 1
+ *
+ * Multiples of 3 and 5
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ * @author dbubenheim
+ */
 public class MultiplesBelow3And5 {
 
     public static void main2(String[] args) {
